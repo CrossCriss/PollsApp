b0931829a95d085a523aaece8e9dfabba0cd3b5e
Viacheslav Herasimyuk <herasimyukviacheslav@gmail.com>
2023-02-25 15:41:18 +0200

    Add test for pub_date

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,18 +2,25 @@ from django.shortcuts import render, get_object_or_404
 from .models import Question, Choice
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 
-def home(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/home.html', context)
+class HomeView(generic.ListView):
+    template_name = 'polls/home.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+    
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
-    return render(request, 'polls/detail.html', context)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
     
 
 def vote(request, question_id):
@@ -26,9 +33,5 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
-    return render(request, 'polls/results.html', context)

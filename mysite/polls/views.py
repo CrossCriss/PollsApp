9cc078adb4e223c226388d9c0a332f65ccab338b
Viacheslav Herasimyuk <herasimyukviacheslav@gmail.com>
2023-02-25 14:15:19 +0200

    Create detail and result votes

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,21 +1,34 @@
-from django.template import loader
-from django.http import HttpResponse
-from .models import Question
+from django.shortcuts import render, get_object_or_404
+from .models import Question, Choice
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 def home(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/home.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/home.html', context)
+
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s" % question_id)
-
-def results(request, question_id):
-    response = "You're loking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/detail.html', context)
+    
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question': question,
+                                                     'error_message': "You didn't select a choice",})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/results.html', context)
